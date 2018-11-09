014f0eaef996b08d0aa2686beccfc1d8241e76a3
Ogulcan Kocabas <44900362+itzAinsworth@users.noreply.github.com>
2018-11-09 17:22:30 +0300

    Added main files

---
+++ iletisim.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>Türev Matematik Index Tasarımı</title>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <div class="sosyal">Bizi takip edin: Facebook - Twitter - Instagram</div>
+<div class="header">
+  <a class="logo" href="../index.php">TÜREV MATEMATİK</a>
+  <div class="menu"><a href="#">YÖNETİCİ PANELİ</a><a href="/iletisim.php">İLETİŞİM</a><a href="#" class="ders_notlari">DERS NOTLARI</a><a href="#" class="hakkimizda">HAKKIMIZDA</a><a href="#">ÖĞRENCİ SİSTEMİ</a><a href="#">ANKET</a></div>
+</div>
+<div id="hakkimizda" class="dropdown">
+  <a href="#">Hakkımızda</a>
+  <a href="#">Misyonumuz</a>
+  <a href="#">Eğitim Kadromuz</a>
+</div>
+<div id="ders_notlari" class="dropdown">
+  <a href="#">Biyoloji</a>
+  <a href="#">Coğrafya</a>
+  <a href="#">Edebiyat</a>
+  <a href="#">Fizik</a>
+  <a href="#">Geometri</a>
+  <a href="#">Kimya</a>
+  <a href="#">Matematik</a>
+  <a href="#">Tarih</a>
+  <a href="#">Türkçe</a>
+</div>
+<div class="slider"><h1>İletişim Adreslerimiz</h1></div>
+<div class="content">
+<p><b>ÇARŞI ŞUBESİ:</b> 0 266 (0245) 40 10</p>
+<p><b>BOZKURT ŞUBESİ:</b> 0 266 (244) 46 40</p>
+<p><b>ÇARŞI ŞUBESİ ADRES:</b> HİSARİÇİ MAH. 1.SÜMERBANK ARKASI SOK. NO:22/B(ALİ ŞUURİ İLKOKULU YANI) KARESİ/BALIKESİR</p>
+<p><b>BOZKURT ŞUBESİ ADRES:</b> Hisariçi Mah. Örücüler Cd. Karanfil Sok. No:20 (Modena Üstü) KARESİ/BALIKESİR</p>
+
+</p>
+</div>
+<div class="footer">
+  <div class="logo">türevmatematik</div>
+  <i>designed by Ainsworth</i><u>2018 - Tüm Hakları Saklıdır. ©</u>
+</div>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
+
+  
+
+    <script  src="js/index.js"></script>
+
+
+
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>Türev Matematik Index Tasarımı</title>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  
+      <link rel="stylesheet" href="css/style.css">
+
+  
+</head>
+
+<body>
+
+  <div class="sosyal">Bizi takip edin: Facebook - Twitter - Instagram</div>
+<div class="header">
+  <a class="logo" href="/index.php">TÜREV MATEMATİK</a>
+  <div class="menu"><a href="#">YÖNETİCİ PANELİ</a><a href="/iletisim.php">İLETİŞİM</a><a href="#" class="ders_notlari">DERS NOTLARI</a><a href="#" class="hakkimizda">HAKKIMIZDA</a><a href="#">ÖĞRENCİ SİSTEMİ</a><a href="#">ANKET</a></div>
+</div>
+<div id="hakkimizda" class="dropdown">
+  <a href="#">Hakkımızda</a>
+  <a href="#">Misyonumuz</a>
+  <a href="#">Eğitim Kadromuz</a>
+</div>
+<div id="ders_notlari" class="dropdown">
+  <a href="#">Biyoloji</a>
+  <a href="#">Coğrafya</a>
+  <a href="#">Edebiyat</a>
+  <a href="#">Fizik</a>
+  <a href="#">Geometri</a>
+  <a href="#">Kimya</a>
+  <a href="#">Matematik</a>
+  <a href="#">Tarih</a>
+  <a href="#">Türkçe</a>
+</div>
+<div id="slider">
+  <a href="#" class="control_next"><i class="fas fa-chevron-right"></i></a>
+  <a href="#" class="control_prev"><i class="fas fa-chevron-left"></i></a>
+  <ul>
+    <li>1. resim 1920x300 min.</li>
+    <li style="background: #aaa;">2. resim 1920x300 min.</li>
+    <li>3. resim 1920x300 min.</li>
+  </ul>  
+</div>
+
+<div class="content">
+  <div class="col">
+    <i class="far fa-user-circle fa-7x"></i>
+    <br><div></div><div></div><div></div><br><h2>UZMAN KADRO</h2><p>Eğitim kadromuz ve branş öğretmenlerimiz alanlarında uzman ve deneyimli öğretmenlerden oluşmaktadır.</p>
+  </div>
+  <div class="col">
+    <i class="fas fa-user-clock fa-7x"></i>
+    <br><div></div><div></div><div></div><br><h2>7/24 EĞİTİM</h2><p>TürevTV yayın kanalımız sayesinde, 7 Gün 24 saat sınıf ortamında anlatılan derslerinizi, istediğiniz yerden izleyebilirsiniz !</p>
+  </div>
+  <div class="col">
+    <i class="fas fa-users fa-7x"></i>
+    <br><div></div><div></div><div></div><br><h2>KALABALIKTA KAYBOLMAYIN!</h2><p>Kurumumuz sınıf dersleri ve etütleri haricinde, en verimli eğitim modeli olan "Birebir Eğitim" modeli ile öğrencilerimizi desteklemektedir.</p>
+  </div>
+</div>
+<div class="footer">
+  <div class="logo">türevmatematik</div>
+  <i>designed by Ainsworth</i><u>2018 - Tüm Hakları Saklıdır. ©</u>
+</div>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
+
+  
+
+    <script  src="js/index.js"></script>
+
+
+
+
+</body>
+
+</html>
